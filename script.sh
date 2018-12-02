8d53e14955c7199c210f47e44fe70e279c830fc6
Vladimir Kruzhkov <morglod@gmail.com>
2018-12-02 07:38:41 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+git init
+npm init
+npm i typescript -g
+tsc --init
+echo "/node_modules\n" > ".gitignore"
+npm i typescript --save-dev
+mkdir src
+mkdir lib
+sed -i -- 's/\/\/\s\"outDir\"\:\s\"\.\/\"/\"outDir\"\: \"\.\/lib\"/g' tsconfig.json
+sed -i -- 's/\/\/\s\"rootDir\"\:\s\"\.\/\"/\"rootDir\"\: \"\.\/src\"/g' tsconfig.json
+sed -i -- 's/\/\/\s\"lib\"\:\s\[/\"lib\"\: \[ \"es2018\" /g' tsconfig.json
+sed -i -- 's/\/\/\s\"declaration\"/\"declaration\"/g' tsconfig.json
