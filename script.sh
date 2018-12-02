55391d7fceabab386f6558395d92dac107fc9660
Vladimir Kruzhkov <morglod@gmail.com>
2018-12-02 07:41:55 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ git init
 npm init
 npm i typescript -g
 tsc --init
-echo "/node_modules\n" > ".gitignore"
+echo /node_modules > ".gitignore"
 npm i typescript --save-dev
 mkdir src
 mkdir lib
@@ -12,3 +12,6 @@ sed -i -- 's/\/\/\s\"outDir\"\:\s\"\.\/\"/\"outDir\"\: \"\.\/lib\"/g' tsconfig.j
 sed -i -- 's/\/\/\s\"rootDir\"\:\s\"\.\/\"/\"rootDir\"\: \"\.\/src\"/g' tsconfig.json
 sed -i -- 's/\/\/\s\"lib\"\:\s\[/\"lib\"\: \[ \"es2018\" /g' tsconfig.json
 sed -i -- 's/\/\/\s\"declaration\"/\"declaration\"/g' tsconfig.json
+git add .
+git commit -m "init"
+echo happy coding! 🙂
